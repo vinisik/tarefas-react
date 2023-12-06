39d8998bdc50cf165106b32d672ea873b9db822f
GabrielPereira128 <145614493+GabrielPereira128@users.noreply.github.com>
2023-12-05 21:23:17 -0300

    Update Sobre.jsx

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -24,7 +24,12 @@ export default function Sobre() {
             <FaArrowLeft /> Voltar
           </Link>
         <h1>Sobre o projeto</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quod tenetur velit qui eveniet vero ab incidunt alias molestiae et repellat at, asperiores accusantium temporibus. Dolorum voluptates repudiandae laboriosam nihil?</p>
+        <p>
+          Este projeto foi desenvolvido por Vinicius siqueira Cardoso e Gabriel Pereira Barcellos Sacramento com o proposito de colocar em prática aquilo aprendido durante as aulas de Desenvolvimento Web em algo que ainda assim possa ser aplicado no dia a dia para ajudar a se organizar.
+        </p>
+        <p>
+          Sendo assim, para o funcionamento do projeto, foi utilizado das rotas da biblioteca React Router Dom, para a criação de rotas e navegação entre as páginas,assim como o uso de Axios para fazer as requisições HTTP para a API.
+        </p>
       </div>
     </div>
       <Footer/>
